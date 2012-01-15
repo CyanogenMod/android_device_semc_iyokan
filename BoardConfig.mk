f021c25639e918eba79831991daf78834be8144b
freexperia <jerpelea@gmail.com>
2012-01-15 12:57:08 +0200

    semc_iyokan: updated device with latest changes

---
+++ BoardConfig.mk
@@ -0,0 +1,4 @@
+-include device/semc/mogami-common/BoardConfigCommon.mk
+-include vendor/semc/iyokan/BoardConfigVendor.mk
+
+TARGET_OTA_ASSERT_DEVICE := MK16i,MK16a,iyokan
---
+++ device_iyokan.mk
@@ -0,0 +1,61 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/semc/iyokan/iyokan-vendor.mk)
+
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := iyokan
+PRODUCT_DEVICE := iyokan
+PRODUCT_MODEL := iyokan
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/semc/iyokan/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+-include device/semc/mogami-common/mogami.mk
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+DISABLE_DEXPREOPT := false
+
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
+DEVICE_PACKAGE_OVERLAYS += device/semc/iyokan/overlay
+
+# These are the hardware-specific configuration files
+#PRODUCT_COPY_FILES += \
+#	device/semc/iyokan/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/iyokan/prebuilt/bootrec:root/sbin/bootrec \
+    device/semc/iyokan/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    device/semc/iyokan/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
+    device/semc/iyokan/recovery.fstab:root/recovery.fstab
+
+
+# semc msm7x30 uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+#Offline charging animation
+PRODUCT_COPY_FILES += \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_01_H.png:system/semc/chargemon/data/charging_animation_01.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_02_H.png:system/semc/chargemon/data/charging_animation_02.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_03_H.png:system/semc/chargemon/data/charging_animation_03.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_H.png:system/semc/chargemon/data/charging_animation_04.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_H.png:system/semc/chargemon/data/charging_animation_05.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_H.png:system/semc/chargemon/data/charging_animation_06.png \
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png
+#    device/semc/msm7x30-common/prebuilt/animations/charging_animation_blank_H.png:system/semc/chargemon/data/charging_animation_blank.png
+
+#LCD DENSITY
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240
