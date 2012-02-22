6a632b00b40e026828b0c6f12f329749d2dab038
freexperia <jerpelea@gmail.com>
2012-02-22 21:47:54 +0200

    semc: initial sensor config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,11 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/iyokan/BoardConfigVendor.mk
 
+BUILD SEMC SENSORS := false
+SENSORS_COMPASS_AK897X := true
+SENSORS_ACCEL_BMA150_INPUT := true
+SENSORS_ACCEL_BMA250_INPUT := false
+SENSORS_PROXIMITY_APDS970X := true
+SENSORS_PRESSURE_BMP180 := false
+
 TARGET_OTA_ASSERT_DEVICE := MK16i,MK16a,iyokan
