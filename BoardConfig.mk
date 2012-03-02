f15cf5230fa0de19a72cad9fb1d1ebf95d820cf9
freexperia <jerpelea@gmail.com>
2012-03-02 04:12:40 +0200

    semc: enables sensor build and prop cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/iyokan/BoardConfigVendor.mk
 
-BUILD SEMC SENSORS := false
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
