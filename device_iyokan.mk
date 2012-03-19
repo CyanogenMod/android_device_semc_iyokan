bba3791baa8851e393ba5d28c144d623fbe3599f
Bin4ry <andreas.makris@googlemail.com>
2012-03-19 21:28:40 +0100

    Add ProParts
    Change-Id: I6380f17855a47f187c18fd12daaedb90be2caaa7

--- device_iyokan.mk
+++ device_iyokan.mk
@@ -26,6 +26,8 @@ DEVICE_PACKAGE_OVERLAYS += device/semc/iyokan/overlay
 #PRODUCT_COPY_FILES += \
 #	device/semc/iyokan/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
 
+PRODUCT_PACKAGES += ProParts
+
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/iyokan/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
