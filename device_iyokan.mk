5f999a5b130354ceae8cfbf697d217c08d5c4f20
Diogo Ferreira <defer@cyanogenmod.com>
2012-03-23 16:30:35 +0000

    iyokan: Build XperiaParts instead of ProParts
    Change-Id: I5a1f7f38071aa41bcb739f3b0467d4c7cfe22a9b

--- device_iyokan.mk
+++ device_iyokan.mk
@@ -26,7 +26,7 @@ DEVICE_PACKAGE_OVERLAYS += device/semc/iyokan/overlay
 #PRODUCT_COPY_FILES += \
 #	device/semc/iyokan/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
 
-PRODUCT_PACKAGES += ProParts
+PRODUCT_PACKAGES += XperiaParts
 
 # Init files
 PRODUCT_COPY_FILES += \
