980bbeec9c0845075fda92bb01c00d867a25f239
freexperia <jerpelea@gmail.com>
2012-09-16 17:33:22 +0300

    iyokan: fix device name for app compatibility check

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := MK16i
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MK16i BUILD_FINGERPRINT="SEMC/MK16i_1251-0386/MK16i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys ro.build.description=MK16i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND="Sony Ericsson" PRODUCT_NAME="Xperia pro" BUILD_FINGERPRINT="SEMC/MK16i_1251-0386/MK16i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys ro.build.description=MK16i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x854
 
