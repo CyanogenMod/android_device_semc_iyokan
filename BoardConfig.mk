eae613e458893c705be947d0083a112e4e5f0549
codeworkx <codeworkx@cyanogenmod.com>
2012-11-14 15:45:49 +0100

    iyokan: device specific part for the new two-stage boot method
    Ported from http://review.cyanogenmod.org/#/c/26440/
    Author: codeworkx <codeworkx@cyanogenmod.com>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ie10e32eb57f777f85b3943a2e86a1bcec6a61141

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,8 @@ TARGET_KERNEL_CONFIG := cyanogen_iyokan_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/iyokan/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+TARGET_RECOVERY_INITRC := device/semc/iyokan/recovery/init.rc
+
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
