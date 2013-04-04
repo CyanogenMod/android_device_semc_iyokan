c2c800eaca0071bc16a198a5d6e1efc6ada870ea
Michael Bestas <mikeioannina@gmail.com>
2013-04-05 01:11:26 +0300

    iyokan: rename defconfig
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ia5c34430d78864ca4263281a48e70ac625fc7152

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
-TARGET_KERNEL_CONFIG := cyanogen_iyokan_defconfig
+TARGET_KERNEL_CONFIG := cm_iyokan_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
 # Recovery
